1394583f605f76f394354b5c50786f9aac7e45d2
William Goodall <wgoodall01@gmail.com>
2016-12-20 18:43:56 -0500

    Don't install nvm if it's installed

--- init.sh
+++ init.sh
@@ -146,25 +146,31 @@ ln_replace .config/nvim
 
 comment "nvm and nodejs:"
 printf "[install] nvm: "
-curl https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh 2>/dev/null | bash &> $LOGS/nvm_install
-if [ "$?" -eq "0" ]; then
-	printf "done\n"
-	
-	# Load nvm
-	NVM_DIR=~/.nvm
-	[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-
-	# Install nodejs stable
-	printf "[install] nodejs stable: "
-	if nvm install stable &>$LOGS/node_install; then
-		printf "done\n"
-	else
-		printf "failed - check logs/node_install"
-	fi
-
+if [ -e ~/.nvm ]; then
+	printf "already installed\n"
 else
-	printf "fail - check logs/nvm_install\n"
+	printf "installing... "
+	curl https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh 2>/dev/null | bash &> $LOGS/nvm_install
+	if [ "$?" -eq "0" ]; then
+		printf "done\n"
+		
+		# Load nvm
+		NVM_DIR=~/.nvm
+		[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+		# Install nodejs stable
+		printf "[install] nodejs stable: "
+		if nvm install stable &>$LOGS/node_install; then
+			printf "done\n"
+		else
+			printf "failed - check logs/node_install"
+		fi
+
+	else
+		printf "fail - check logs/nvm_install\n"
+	fi
 fi
+
 
 comment "Other stuff:"
 install htop
