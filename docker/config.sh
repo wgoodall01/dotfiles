bfcafc855392bf42f6306c9b668d7c08ed97f5b3
William Goodall <wgoodall01@gmail.com>
2018-11-23 21:38:40 -0500

    docker: add language config flags

--- docker/config.sh
+++ docker/config.sh
@@ -1,8 +1,15 @@
-#1!/usr/bin/env bash
+#!/usr/bin/env bash
 
 export CFG_GUI=false   # no GUI in container
 export CFG_SSH=false   # don't decrypt 
 export CFG_CLOUD=true  # Enable installing of cloud stuff?
+
+# Language configuration
+export CFG_LANG_NODEJS=true 
+export CFG_LANG_GOLANG=true
+export CFG_LANG_RUBY=false
+export CFG_LANG_JAVA=false
+export CFG_LANG_CPP=true
 
 # Disabled, this should be set on first boot in container
 export CRYPTO_PW="" 
